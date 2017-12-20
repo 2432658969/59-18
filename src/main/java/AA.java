60b7ef3298528d6e2349e3d634dc013b11e5c1bc
11324 <11324@192.168.1.185>
2017-12-20 19:25:59 +0800

    haha

---
+++ src/main/java/AA.java
@@ -0,0 +1,25 @@
+/**
+ * Project Name:59-18
+ * File Name:AA.java
+ * Package Name:
+ * Date:2017年12月20日下午7:14:47
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+/**
+ * Description:   <br/>
+ * Date:     2017年12月20日 下午7:14:47 <br/>
+ * @author   11324
+ * @version
+ * @see
+ */
+public class AA {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+
+    }
+
+}
+
